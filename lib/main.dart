ea8472c104118eb7d4578888d43072fbef60a740
selpeapi <selpeapi@gmail.com>
2020-10-04 17:27:02 +0900

    디버그 라벨 제거 문구 추가 +줄바꿈

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Center(
-          child: Text("ITZY, YEJI"),
-        )),
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("ITZY, YEJI"),
+      ),
+    ),
   );
 }
